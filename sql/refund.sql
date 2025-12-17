10465de4811ce6052b4f1ed92930110d6b3ccdc2
shyamu-furlenco <shyamu.yadav@furlenco.com>
2025-12-17 07:28:39 +0000

    final changes

---
+++ sql/refund.sql
@@ -0,0 +1,7 @@
+%sql
+SELECT * 
+FROM  `furlenco_silver`.`refunds_evolve`.`refund_transactions` 
+WHERE 1=1 
+AND (created_at + interval '330 minutes') >= DATE('2025-04-01') 
+AND (created_at + interval '330 minutes') < DATE('2025-12-01')
+LIMIT 1000
