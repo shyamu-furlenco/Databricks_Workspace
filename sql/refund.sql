60957e420fa16d03990a6e6f3e5abf6bdc9d5fa0
shyamu-furlenco <shyamu.yadav@furlenco.com>
2025-12-17 12:59:37 +0530

    remove sql directory

--- sql/refund.sql
+++
@@ -1,7 +0,0 @@
-%sql
-SELECT * 
-FROM  `furlenco_silver`.`refunds_evolve`.`refund_transactions` 
-WHERE 1=1 
-AND (created_at + interval '330 minutes') >= DATE('2025-04-01') 
-AND (created_at + interval '330 minutes') < DATE('2025-12-01')
-LIMIT 1000
