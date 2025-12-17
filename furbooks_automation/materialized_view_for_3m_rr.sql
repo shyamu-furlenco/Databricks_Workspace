60087544cf0e66fb3e6ab26fe498ef72aaa66635
shyamu-furlenco <shyamu.yadav@furlenco.com>
2025-12-17 18:30:03 +0530

    revenue add

--- furbooks_automation/materialized_view_for_3m_rr.sql
+++ furbooks_automation/materialized_view_for_3m_rr.sql
@@ -1,7 +1,16 @@
 create materialized view furlenco_silver.furlenco_analytics.furbooks_snapshot_movement_3m as 
-SELECT * 
+SELECT id, recognition_type,  accountable_entity_id, accountable_entity_type,
+            start_date, end_date, to_be_recognised_on, recognised_at, 
+            external_reference_type, external_reference_id,
+            monetary_components, created_at, updated_at,
+            monetary_components_postTaxAmount, 
+            monetary_components_taxableAmount 
 FROM furlenco_silver.furbooks_evolve.revenue_recognitions
 WHERE state not in ('CANCELLED', 'INVALIDATED')
 AND vertical = 'FURLENCO_RENTAL'
-AND created_at + interval '330 minutes' >= DATE('2025-10-01')
-AND created_at + interval '330 minutes' < DATE('2025-12-01')
+AND start_date >= DATE('2025-09-01')
+AND start_date < DATE('2025-12-01')
+
+
+
+
--- furbooks_automation/revenue_cal.sql
+++ furbooks_automation/revenue_cal.sql
@@ -1,2 +1,12 @@
-SELECT *
+with normal_billing as (
+SELECT  DATE_TRUNC('month', start_date) as month_start,
+        monetary_components_taxableAmount as taxable_amount
 FROM furlenco_silver.furlenco_analytics.furbooks_snapshot_movement_3m
+WHERE accountable_entity_type in ('ATTACHMENT','ITEM')
+AND external_reference_type  <> 'RETURN'
+)
+SELECT month_start,
+       SUM(taxable_amount) as total_taxable_amount
+FROM normal_billing
+GROUP BY month_start
+ORDER BY month_start desc;
