534473a596d2ec835894c1789fcf4eea5ef53a59
shyamu-furlenco <shyamu.yadav@furlenco.com>
2025-12-17 15:35:11 +0530

    create mv view

--- furbooks_automation/materialized_view_for_3m_rr.sql
+++ furbooks_automation/materialized_view_for_3m_rr.sql
@@ -1,2 +1,7 @@
-select * from `furlenco_silver`.furbooks_evolve.revenue_recognitions
-limit 100
+create materialized view furlenco_silver.furlenco_analytics.furbooks_snapshot_movement_3m as 
+SELECT * 
+FROM furlenco_silver.furbooks_evolve.revenue_recognitions
+WHERE state not in ('CANCELLED', 'INVALIDATED')
+AND created_at + interval '330 minutes' >= DATE('2025-10-01')
+AND created_at + interval '330 minutes' < DATE('2025-12-01')
+
